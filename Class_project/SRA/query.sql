9af7c6e286be6fac784f9f7d5e30af16d43a942c
chriscrsmith <chriscs@uoregon.edu>
2026-02-10 12:27:51 -0500

    new query

--- Class_project/SRA/query.sql
+++ Class_project/SRA/query.sql
@@ -1,22 +1,5 @@
--- SELECT
--- 	bioproject, organism, attributes
--- FROM
--- 	nih-sra-datastore.sra.metadata
--- WHERE
--- 	consent = "public"
--- 	AND bioproject IS NOT NULL
--- 	AND assay_type = "WGS"
--- 	AND platform = "ILLUMINA"
--- 	AND librarysource = "GENOMIC"
--- 	AND organism != "Homo sapiens"
--- 	AND organism != "unidentified"
--- 	AND not organism LIKE '%metagenom%'
--- 	AND NOT EXISTS (SELECT 1 FROM UNNEST(attributes) AS a WHERE a.k LIKE '%host%')
--- LIMIT 1000000;
-
-
 SELECT
-	acc, bioproject, organism
+	acc, bioproject, organism--, attributes
 FROM
 	nih-sra-datastore.sra.metadata
 WHERE
@@ -29,6 +12,15 @@ WHERE
 	AND organism != "Homo sapiens"
 	AND organism != "unidentified"
 	AND not organism LIKE '%metagenom%'
-	AND NOT EXISTS (SELECT 1 FROM UNNEST(attributes) AS a WHERE a.k LIKE '%host%')
-
-
+	AND NOT EXISTS (SELECT 1 FROM UNNEST(attributes) AS a WHERE LOWER(a.k) LIKE '%host%')
+    AND EXISTS (
+        SELECT 1
+        FROM UNNEST(attributes) AS a
+        WHERE LOWER(a.k) LIKE '%lat%' AND LOWER(a.v) NOT IN ('na', 'not provided', '', 'unknown', 'not applicable') AND a.v IS NOT NULL
+    )
+    AND EXISTS (
+        SELECT 1
+        FROM UNNEST(attributes) AS a
+        WHERE LOWER(a.k) LIKE '%lon%' AND LOWER(a.v) NOT IN ('na', 'not provided', '', 'unknown', 'not applicable') AND a.v IS NOT NULL
+    )
+--LIMIT 1000;
