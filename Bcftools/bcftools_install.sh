f9e92b952249f5f8ed9c2110f6d14bf17510cb71
Chris Smith <chriscs@login1.hsn0.cm.bigred200.uits.iu.edu>
2026-02-26 17:57:39 -0500

    bcftools install script

---
+++ Bcftools/bcftools_install.sh
@@ -0,0 +1,19 @@
+
+# edit this if you want
+install_dir=/N/u/$USER/Software/
+echo $install_dir
+
+mkdir -p $install_dir
+cd $install_dir
+git clone --recurse-submodules https://github.com/samtools/htslib.git
+git clone https://github.com/samtools/bcftools.git
+cd bcftools
+autoheader && autoconf
+./configure --prefix=$install_dir/bcftools/
+make
+
+# add to bashrc
+echo >> ~/.bashrc
+echo "# bcftools" >> ~/.bashrc
+echo PATH=\$PATH:$install_dir/bcftools/ >> ~/.bashrc
+
