07af2e53908837059c3ff19d3d5eb2760b3b74a9
Chris Smith <chriscs@login1.hsn0.cm.bigred200.uits.iu.edu>
2026-03-26 09:13:27 -0400

    arb vcf, arb k

--- ConStruct/construct.R
+++ ConStruct/construct.R
@@ -1,26 +1,32 @@
+
+### settings and command line args
+print("loading software")
+library("conStruct")  #install.packages("conStruct")
+library(geosphere)  #install.packages("geosphere")
+args <- commandArgs(trailingOnly = TRUE)  # print(args)
 setwd("./")
-#install.packages("conStruct")
-library("conStruct")
-#install.packages("geosphere", type = "binary")
-library(geosphere)
 
 
-#vignette(topic="format-data",package="conStruct")
-
-genos = read.table("snps_final.012", row.names = 1, header=F)
+### read and preprocess  #vignette(topic="format-data",package="conStruct")
+print("reading vcf")
+fp = paste(args[1], ".012", sep="")
+genos = read.table(fp, row.names = 1, header=F)
 freqs = genos / 2  # convert allele counts to frequencies in each indiv
 freqs = as.matrix(freqs)
 dim(freqs)
-locs = read.table("coords.txt", header=F)
+
+print("reading locs")
+fp = paste(args[1], ".locs", sep="")
+locs = read.table(fp, header=F)
 locs = locs[, c(1,2)]
 locs = as.matrix(locs)
 dim(locs)
 dists <- distm(locs, fun = distHaversine)
 dim(dists)
 
-
+K=as.integer(args[2])
 my.run <- conStruct(spatial = F, 
-                    K = 2, 
+                    K = K, 
                     freqs = freqs,
                     geoDist = dists,
                     coords = locs,
