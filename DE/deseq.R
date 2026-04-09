00c35cb3204f096e95b2ee68b3440f17d92edfca
Chris Smith <chriscs@login1.hsn0.cm.bigred200.uits.iu.edu>
2026-04-09 07:24:24 -0400

    new length correction

--- DE/deseq.R
+++ DE/deseq.R
@@ -16,12 +16,27 @@ gene_map = read.table(
 #
 samples = read.table("samples.txt", header=T)  # tab-sep: accession, treatment, file
 
-data = tximport(samples$file, type = "salmon", tx2gene = gene_map, countsFromAbundance="lengthScaledTPM")
+
+
 # https://github.com/thelovelab/tximport/blob/devel/R/helper.R#L26
 #     newCounts <- abundanceMat * rowMeans(lengthMat)
-#      reverses the length normalization.
+#     Seems to rescale back to counts-like scale/units;
+#     but it uses mean, therefore length-corrected counts: L * (mean length / transcript length)
 # https://www.bioconductor.org/packages/devel/bioc//vignettes/tximport/inst/doc/tximport.html
 #     ""lengthScaledTPM". Using either of these approaches, the counts are not correlated with length'
+#data = tximport(samples$file, type = "salmon", tx2gene = gene_map, countsFromAbundance="lengthScaledTPM")
+
+# But: the DESeq2 docs doesn't say to use countsFromAbundance in the salmon vignette.
+#     Corrects for sample-specific gene length using "offsets".
+#     e.g., data$length
+# https://bioconductor.posit.co/packages/3.22/bioc/manuals/DESeq2/man/DESeq2.pdf
+#     DESeq() doesn't take an offsets argument
+# https://github.com/thelovelab/DESeq2/blob/devel/R/AllClasses.R#L419
+#     DESeqDataSetFromTximport() automaticaly uses data$length
+#     lengths <- txi$length
+# Conclusion: both approaches address a similar goal; deseq2 says to do it this way.
+data = tximport(samples$file, type = "salmon", tx2gene = gene_map)
+
 
 samples$treatment = factor(samples$treatment)
 
@@ -37,34 +52,16 @@ resultsNames(dds) # lists the coefficient names- need this to match below
 
 res = results(dds, name="treatment_infected_vs_control")
 
-resOrdered <- res[order(res$padj), ]
+resOrdered = res[order(res$padj), ]
 head(resOrdered)
 
-#plotMA(res, main="DESeq2", ylim=c(-5,5))
-
-# Prepare data
-resOrdered <- as.data.frame(resOrdered)
-resOrdered$gene <- rownames(resOrdered)
-
-# Define significance: padj < 0.05 and |log2FC| > 1
-sig <- resOrdered$padj < 0.05 & abs(resOrdered$log2FoldChange) > 1
-
-# Volcano plot
+# plot
+resOrdered = as.data.frame(resOrdered)
+resOrdered$gene = rownames(resOrdered)
+sig = resOrdered$padj < 0.05 & abs(resOrdered$log2FoldChange) > 1
 plot(resOrdered$log2FoldChange, -log10(resOrdered$padj),
      pch = 20,
      col = ifelse(sig, "red", "grey"),
      xlab = "Log2 Fold Change",
      ylab = "-Log10 Adjusted p-value",
      main = "Volcano Plot")
-
-# Optionally, highlight top genes
-top_genes <- head(resOrdered[order(resOrdered$padj), ], 10)
-text(top_genes$log2FoldChange, -log10(top_genes$padj),
-     labels = top_genes$gene,
-     pos = 3, cex = 0.8, col = "blue")
-
-
-
-
-
-
