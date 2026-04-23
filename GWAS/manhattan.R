4cf284527a1bda3886ee48afdb12166bf2c6d4ef
chriscrsmith <chriscs@iu.edu>
2026-04-23 09:14:39 -0400

    manhattan plot

---
+++ GWAS/manhattan.R
@@ -0,0 +1,23 @@
+args <- commandArgs(trailingOnly = TRUE)  # print(args)
+inpath=args[1]
+
+data <- read.table(inpath, header=TRUE)
+chr_len <- aggregate(BP ~ CHR, data, max)
+chr_len$offset <- c(0, cumsum(chr_len$BP[-nrow(chr_len)]))
+data <- merge(data, chr_len[, c("CHR", "offset")], by="CHR")
+data$BPcumul <- data$BP + data$offset
+data$logP <- -log10(data$P)
+
+png(paste(inpath, ".png", sep=""), width = 4500, height = 1500, res = 300, type = "cairo")
+plot(data$BPcumul, data$logP,
+     pch = 20,
+     col = as.factor(data$CHR),
+     xlab = "Genomic position",
+     ylab = "-log10(P)",
+     xaxt="n")
+abline(8,0, lty=2)
+axis(1,
+at = tapply(data$BPcum, data$CHR, mean),
+labels = unique(data$CHR))
+dev.off()
+
